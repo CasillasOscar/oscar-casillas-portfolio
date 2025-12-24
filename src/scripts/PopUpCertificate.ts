7a649e6f9a67c035eda1e151ab5efb656ef9ebe9
CasillasOscar <oscarcasillasrodriguez@gmail.com>
2025-12-24 13:15:26 +0100

    feat: Validación de imagen de certificado diferente de null

--- src/scripts/PopUpCertificate.ts
+++ src/scripts/PopUpCertificate.ts
@@ -1,5 +1,6 @@
 const dialog = document.getElementById("certificate-modal") as HTMLDialogElement;
 const img = document.getElementById("modal-image") as HTMLImageElement;
+const text = document.getElementById("modal-text") as HTMLElement;
 const closeBtn = document.getElementById("close-modal");
 
 // Cerrar modal
@@ -13,8 +14,16 @@ document.addEventListener("click", (e) => {
   const target = e.target as HTMLElement;
   const button = target.closest(".view-certificate-btn") as HTMLElement;
 
-  if (button && button.dataset.image) {
-    img.src = button.dataset.image;
+  if (button) {
+    const imageSrc = button.dataset.image;
+    if (imageSrc) {
+      img.src = imageSrc;
+      img.style.display = "block";
+      if (text) text.style.display = "none";
+    } else {
+      img.style.display = "none";
+      if (text) text.style.display = "block";
+    }
     dialog.showModal();
   }
 });
